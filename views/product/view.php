028c6358f55c8edd39c6633a2993054a289eb8e4
Anastasiya Mudraya <mudraya.od@gmail.com>
2019-04-29 20:42:31 +0300

    img load product view

--- views/product/view.php
+++ views/product/view.php
@@ -1,25 +1,17 @@
 <?php
+
+/* @var $this yii\web\View */
 use yii\helpers\Html;
 ?>
-
 <section>
     <div class="container">
         <div class="row">
             <div class="col-sm-3">
                 <div class="left-sidebar">
                     <h2>Category</h2>
-                    <div class="panel-group category-products " id="accordian"><!--category-productsr-->
-                        <div class="panel panel-default">
-                            <div class="panel-heading">
-                                <h4 class="panel-title">
-                                    <ul class="catalog ">
-                                        <?= \app\components\MenuWidget::Widget(['tpl' => 'menu']) ?>
-                                    </ul>
-                                    <!--                            --><?php //debug($data); ?>
-                                </h4>
-                            </div>
-                        </div>
-                    </div>
+                    <ul class="catalog category-products">
+                        <?= \app\components\MenuWidget::widget(['tpl' => 'menu'])?>
+                    </ul>
 
                     <div class="brands_products"><!--brands_products-->
                         <h2>Brands</h2>
@@ -51,36 +43,31 @@ use yii\helpers\Html;
                 </div>
             </div>
 
-<!--            --><?php //debug($product) ?>
-<!--            --><?php //debug($product['category']) ?>
+            <?php
+            $mainImg = $product->getImage();
+            $gallery = $product->getImages();
+            ?>
 
             <div class="col-sm-9 padding-right">
                 <div class="product-details"><!--product-details-->
                     <div class="col-sm-5">
                         <div class="view-product">
-                            <?= Html::img("@web/images/products/{$product->img}", ['alt' => $product->name]) ?>
+                            <img src='<?=$mainImg->getUrl('250x250')?>'>
                             <h3>ZOOM</h3>
                         </div>
                         <div id="similar-product" class="carousel slide" data-ride="carousel">
 
                             <!-- Wrapper for slides -->
                             <div class="carousel-inner">
-                                <div class="item active">
-                                    <a href=""><img src="/images/product-details/similar1.jpg" alt=""></a>
-                                    <a href=""><img src="/images/product-details/similar2.jpg" alt=""></a>
-                                    <a href=""><img src="/images/product-details/similar3.jpg" alt=""></a>
-                                </div>
-                                <div class="item">
-                                    <a href=""><img src="/images/product-details/similar1.jpg" alt=""></a>
-                                    <a href=""><img src="/images/product-details/similar2.jpg" alt=""></a>
-                                    <a href=""><img src="/images/product-details/similar3.jpg" alt=""></a>
-                                </div>
-                                <div class="item">
-                                    <a href=""><img src="/images/product-details/similar1.jpg" alt=""></a>
-                                    <a href=""><img src="/images/product-details/similar2.jpg" alt=""></a>
-                                    <a href=""><img src="/images/product-details/similar3.jpg" alt=""></a>
-                                </div>
-
+                                <?php $count = count($gallery); $i = 0; foreach($gallery as $img): ?>
+                                    <?php if($i % 3 == 0):?>
+                                        <div class="item <?php if($i == 0) echo ' active'?>">
+                                    <?php endif;?>
+                                    <a href=""><?= Html::img($img->getUrl('84x85'), ['alt' => ''])?></a>
+                                    <?php $i++; if($i % 3 == 0 || $i == $count):?>
+                                        </div>
+                                    <?php endif;?>
+                                <?php endforeach;?>
                             </div>
 
                             <!-- Controls -->
@@ -95,34 +82,34 @@ use yii\helpers\Html;
                     </div>
                     <div class="col-sm-7">
                         <div class="product-information"><!--/product-information-->
-                            <?php if($product->new) : ?>
-                                <?= \yii\helpers\Html::img("@web/images/home/new.png", ['alt' => 'Новинка', 'class' => 'newarrival']) ?>
-                            <?php endif; ?>
-
-                            <?php if($product->sale) : ?>
-                                <?= \yii\helpers\Html::img("@web/images/home/sale.png", ['alt' => 'Распродажа', 'class' => 'newarrival']) ?>
-                            <?php endif; ?>
-                            <h2><?= $product->name ?></h2>
+                            <?php if($product->new): ?>
+                                <?= Html::img("@web/images/home/new.png", ['alt' => 'Новинка', 'class' => 'newarrival'])?>
+                            <?php endif?>
+                            <?php if($product->sale): ?>
+                                <?= Html::img("@web/images/home/sale.png", ['alt' => 'Распродажа', 'class' => 'newarrival'])?>
+                            <?php endif?>
+                            <h2><?= $product->name?></h2>
                             <p>Web ID: 1089772</p>
                             <img src="/images/product-details/rating.png" alt="" />
                             <span>
-									<span><?= $product->price ?></span>
+									<span>US $<?= $product->price?></span>
 									<label>Quantity:</label>
 									<input type="text" value="1" id="qty" />
-                                <a href="<?= \yii\helpers\Url::to(['cart/add', 'id' => $product->id])?>" data-id="<?= $product->id?>" class="btn btn-fefault add-to-cart cart">
-										<i class="fa fa-shopping-cart"></i>
-										Add to cart
-									</a>
+									<a href="<?= \yii\helpers\Url::to(['cart/add', 'id' => $product->id])?>" data-id="<?= $product->id?>" class="btn btn-fefault add-to-cart cart">
+                                        <i class="fa fa-shopping-cart"></i>
+                                        Add to cart
+                                    </a>
 								</span>
                             <p><b>Availability:</b> In Stock</p>
                             <p><b>Condition:</b> New</p>
-                            <p><b>Brand:</b> E-SHOPPER</p>
+                            <p><b>Brand:</b> <a href="<?= \yii\helpers\Url::to(['category/view', 'id' => $product->category->id]) ?>"><?= $product->category->name?></a></p>
                             <a href=""><img src="/images/product-details/share.png" class="share img-responsive"  alt="" /></a>
+                            <?= $product->content?>
                         </div><!--/product-information-->
                     </div>
                 </div><!--/product-details-->
 
-                <div class="category-tab shop-details-tab"><!--category-tab-->
+                <!--<div class="category-tab shop-details-tab">
                     <div class="col-sm-12">
                         <ul class="nav nav-tabs">
                             <li><a href="#details" data-toggle="tab">Details</a></li>
@@ -296,10 +283,10 @@ use yii\helpers\Html;
                                 <p><b>Write Your Review</b></p>
 
                                 <form action="#">
-										<span>
-											<input type="text" placeholder="Your Name"/>
-											<input type="email" placeholder="Email Address"/>
-										</span>
+                                                        <span>
+                                                            <input type="text" placeholder="Your Name"/>
+                                                            <input type="email" placeholder="Email Address"/>
+                                                        </span>
                                     <textarea name="" ></textarea>
                                     <b>Rating: </b> <img src="/images/product-details/rating.png" alt="" />
                                     <button type="button" class="btn btn-default pull-right">
@@ -310,40 +297,34 @@ use yii\helpers\Html;
                         </div>
 
                     </div>
-                </div><!--/category-tab-->
+                </div>-->
+                <!--/category-tab-->
 
-                <div class="recommended_items"><!--recommended_items-->
+                <div class="recommended_items">
+                    <!--recommended_items-->
                     <h2 class="title text-center">recommended items</h2>
 
                     <div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
-                        <div class="a-inner">
-
-                            <?php
-                            $count = count($hits);
-                            $i =0;
-                            foreach($hits as $hit) :
-                            if ($i % 3 == 0):
-                            ?>
-                            <div class="item <?php if ($i==0) echo 'active' ?>">
+                        <div class="carousel-inner">
+                            <?php $count = count($hits); $i = 0; foreach($hits as $hit): ?>
+                                <?php if($i % 3 == 0): ?>
+                                    <div class="item <?php if($i == 0) echo 'active' ?>">
                                 <?php endif; ?>
                                 <div class="col-sm-4">
                                     <div class="product-image-wrapper">
                                         <div class="single-products">
                                             <div class="productinfo text-center">
-                                                <?= \yii\helpers\Html::img("@web/images/products/{$hit->img}", ['alt' => $hit->name]) ?>
+                                                <?= Html::img("@web/images/products/{$hit->img}", ['alt' => $hit->name])?>
                                                 <h2>$<?= $hit->price?></h2>
-                                                <p><a href="<?= \yii\helpers\Url::to(['product/view', 'id' => $hit->id]) ?>"><?= $hit->name?></a></p>
+                                                <p><a href="<?= \yii\helpers\Url::to(['product/view', 'id' => $hit->id])?>"><?= $hit->name?></a></p>
                                                 <button type="button" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            <?php
-                            $i++;
-                            if($i % 3 == 0 || $i == $count):
-                            ?>
-                            </div>
-                            <?php endif; ?>
+                                <?php $i++; if($i % 3 == 0 || $i == $count): ?>
+                                    </div>
+                                <?php endif; ?>
                             <?php endforeach; ?>
                         </div>
                         <a class="left recommended-item-control" href="#recommended-item-carousel" data-slide="prev">
